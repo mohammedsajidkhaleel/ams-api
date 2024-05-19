27284546b12f83f55718556715233d04b0f468be
Mohammed Sajid Khaleel <m.khaleel@smartappholding.com>
2024-05-19 18:10:57 +0300

    adding convert to asset

---
+++ ams.application/ItemReceipts/ConvertToAsset/ConvertToAssetCommand.cs
@@ -0,0 +1,6 @@
+﻿using ams.application.Abstractions.Messaging;
+
+namespace ams.application.ItemReceipts.ConvertToAsset;
+
+public sealed record ConvertToAssetCommand() : 
+    IQuery<List<Guid>>;
---
+++ ams.application/ItemReceipts/ConvertToAsset/ConvertToAssetCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using ams.application.Abstractions.Messaging;
+using ams.domain.Abstractions;
+using ams.domain.ItemReceipts;
+
+namespace ams.application.ItemReceipts.ConvertToAsset;
+
+//internal sealed class ConvertToAssetCommandHandler
+//    : ICommandHandler<ConvertToAssetCommand, List<Guid>>
+//{
+//    private readonly Item Repository _itemReceiptRepository;
+//    private readonly IUnitOfWork _unitOfWork;
+//    public Task<Result<List<Guid>>> Handle(ConvertToAssetCommand request, CancellationToken cancellationToken)
+//    {
+        
+//    }
+//}
